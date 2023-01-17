f2783f11e0bf936e6036e101d04ad99a70b37812
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-01-17 03:39:02 +0300

    all test passed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, {useState, useEffect} from "react"
+
+function App(){
+    const [image, setImage]=useState("")
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response=>response.json())
+        .then(data=> setImage(data.message))
+    },[]);
+    if (!image){
+        return <p>Loading...</p>
+    }
+    return  (
+    <div>
+        <img src={image} alt="A Random Dog"></img>
+    </div>
+    );
+}
+export default App;
